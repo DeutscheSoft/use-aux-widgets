8b5c8b26c342fab06d0c1f5eaf3d9f2d7e81bdc8
Arne Goedeke <arne@deuso.de>
2023-03-07 15:36:05 +0100

    useWidgets: Fix several issues
    The array of widgets was not correctly returned. Also, using the Memo
    did not work as intended. The widgets array is now stored inside of a
    state and updated whenever a widget is added or removed.

--- src/useWidgets.js
+++ src/useWidgets.js
@@ -1,5 +1,9 @@
-import { useMemo, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { forEachChangedProperty } from './forEachChangedProperty.js';
+
+function createWidget(Widget, options) {
+  return new Widget(Object.assign({}, options));
+}
 
 /**
  * Creates a list of widgets from the given options.
@@ -11,46 +15,64 @@ import { forEachChangedProperty } from './forEachChangedProperty.js';
  */
 export function useWidgets(Widget, options) {
   const optionsRef = useRef(options);
-  const widgets = useMemo(
-    () => optionsRef.current.map((options) => new Widget(options)),
-    [ Widget ]
-  );
+  const widgetsRef = useRef([]);
+  const [ widgets, setWidgets ] = useState(widgetsRef.current);
 
-  const updateCount = Math.min(options.length, widgets.length);
-  const currentOptions = optionsRef.current;
+  // Create or destroy widgets based on the length of options and update
+  // options if they changed.
+  useEffect(() => {
+    const currentOptions = optionsRef.current;
+    let widgets = widgetsRef.current;
+    const updateCount = Math.min(options.length, widgets.length);
 
-  for (let i = 0; i < updateCount; i++) {
-    const widget = widgets[i];
+    if (options.length !== widgets.length || widgets.some((widget) => !(widget instanceof Widget))) {
+      // create a copy
+      widgets = widgets.slice(0);
+    }
 
-    forEachChangedProperty(
-      currentOptions[i],
-      options[i],
-      (name) => {
-        widget.reset(name);
-      },
-      (name, value, prevValue) => {
-        widget.set(name, value);
+    for (let i = 0; i < updateCount; i++) {
+      const widget = widgets[i];
+      if (!(widget instanceof Widget)) {
+        widget.destroy();
+        widgets[i] = createWidget(Widget, options[i]);
+      } else {
+        forEachChangedProperty(
+          currentOptions[i],
+          options[i],
+          (name) => {
+            widget.reset(name);
+          },
+          (name, value, prevValue) => {
+            widget.set(name, value);
+          }
+        );
       }
-    );
-  }
-
-  if (updateCount < widgets.length) {
-    for (let i = updateCount; i < widgets.length; i++) {
-      widgets[i].destroy();
     }
-    widgets.length = updateCount;
-  } else if (updateCount < options.length) {
-    widgets.length = options.length;
-    for (let i = updateCount; i < options.length; i++) {
-      widgets[i] = new Widget(options[i]);
-    }
-  }
 
-  optionsRef.current = options;
+    if (updateCount < widgets.length) {
+      for (let i = updateCount; i < widgets.length; i++) {
+        widgets[i].destroy();
+      }
+      widgets.length = updateCount;
+    } else if (updateCount < options.length) {
+      widgets.length = options.length;
+      for (let i = updateCount; i < options.length; i++) {
+        widgets[i] = new Widget(Object.assign({}, options[i]));
+      }
+    }
+
+    optionsRef.current = options;
+
+    if (widgetsRef.current !== widgets) {
+      widgetsRef.current = widgets;
+      setWidgets(widgets);
+    }
+  }, [ Widget, options, setWidgets ]);
 
   useEffect(() => {
     return () => {
-      widgets.forEach((widget) => widget.destroy());
     };
-  }, [ widgets ]);
+  }, []);
+
+  return widgets;
 }
