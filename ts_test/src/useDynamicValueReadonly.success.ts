ba4c78329c5153fe537cb19d4afe043163cc61d2
Arne Goedeke <arne@deuso.de>
2023-10-17 17:26:33 +0200

    [testsuite]: Add typescript test

--- ts_test/src/useDynamicValueReadonly.success.ts
+++ ts_test/src/useDynamicValueReadonly.success.ts
@@ -7,3 +7,23 @@ export function run(v: DynamicValue<number>): number {
 
   return foo + 2;
 }
+
+export function run2(v: DynamicValue<number>): number {
+  const foo = useDynamicValueReadonly(v, 12);
+
+  return foo + 2;
+}
+
+export function run3(v: DynamicValue<number> | null): number {
+  const foo = useDynamicValueReadonly(v);
+
+  return foo + 2;
+}
+
+export function run4(): number {
+  useDynamicValueReadonly(null);
+  useDynamicValueReadonly(undefined);
+  const foo = useDynamicValueReadonly(undefined, 23);
+
+  return foo + 2;
+}
