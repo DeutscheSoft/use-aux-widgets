565aa70bbc58c8efe899083f8c5f24650ed0db72
Arne Goedeke <arne@deuso.de>
2023-12-04 16:49:59 +0100

    [testsuite]: Add test for useWidgetBinding()

---
+++ test/tests/useWidgetBinding.test.js
@@ -0,0 +1,46 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { useWidget } from './src/useWidget.js';
+import { useWidgetBinding } from './src/useWidgetBinding.js';
+import { strictEqual, deepEqual } from 'node:assert';
+import { WidgetMock } from './WidgetMock.js';
+import { DynamicValue } from '@deutschesoft/awml/src/dynamic_value.js';
+import test from 'node:test';
+
+test('useWidgetBinding', () => {
+  {
+    const options = { foo: 3 };
+    const Widget = WidgetMock;
+    const backendValue = DynamicValue.fromConstant(1);
+    let bindings = {
+      name: 'foo',
+      backendValue,
+    };
+    const { result, rerender } = renderHook(() => {
+      const widget = useWidget(Widget, options);
+      useWidgetBinding(widget, bindings);
+      return widget;
+    });
+
+    const w = result.current;
+    strictEqual(typeof w, 'object');
+    deepEqual({ foo: 1 }, w.options);
+
+    backendValue.set(3);
+    rerender();
+    deepEqual({ foo: 3 }, w.options);
+
+    bindings = [
+      bindings,
+      {
+        name: 'bar',
+        backendValue,
+      }
+    ];
+    rerender();
+    deepEqual({ foo: 3, bar: 3 }, w.options);
+
+    backendValue.set(4);
+    rerender();
+    deepEqual({ foo: 4, bar: 4 }, w.options);
+  }
+});
