874c35d259776fe0baec23d989ab8329d2a8e52f
Arne Goedeke <arne@deuso.de>
2023-12-06 16:44:11 +0100

    useWidgetsWithBindingsAndEvents: Fixed import

--- src/useWidgetsWithBindingsAndEvents.js
+++ src/useWidgetsWithBindingsAndEvents.js
@@ -1,11 +1,11 @@
 import { useWidgets } from './useWidgets.js';
-import { useWidgetBindings } from './useWidgetBindings.js';
+import { useWidgetsBindings } from './useWidgetsBindings.js';
 import { useWidgetsEvents } from './useWidgetsEvents.js';
 
 export function useWidgetsWithBindingsAndEvents(Widget, widgetOptions, bindingDescriptions, events) {
   const widgets = useWidgets(Widget, widgetOptions);
 
-  useWidgetBindings(widgets, bindingDescriptions);
+  useWidgetsBindings(widgets, bindingDescriptions);
   useWidgetsEvents(widgets, events);
 
   return widgets;
